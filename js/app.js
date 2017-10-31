73ca9f277de4bd78594345185b7885bdac2181ad
fernando <fernandosrojo@yahoo.com>
2017-10-30 22:49:22 -0400

    fixed scrolling

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-    var pantryUrl = 'https://api.yummly.com/v1/api/recipes?_app_id=d8ff7ac7&_app_key=a07332a882b586c48dadbbd230308af1';
+    var pantryUrl = 'https://api.yummly.com/v1/api/recipes?_app_id=d8ff7ac7&_app_key=a07332a882b586c48dadbbd230308af1&maxResult=20';
 
     // highlight the search bar
     $('body').find('.search input').focus();
-    
+
     function getRecipes(url) {
         $.getJSON(url, function (data) {
             var recipe = data.matches;
             console.log(recipe);
             if (recipe.length === 0) {
-                $('.Output').append('<p>Shoot...no matches for that search.</p>');
+                $('.Output').html('<p>Shoot...no matches for that search. 😕</p>');
             } else {
                 $('.Output').html('<h2>Your pantry created these delicious recipes.</h2>');
                 $.each(recipe, function () {
@@ -19,9 +19,18 @@ $(document).ready(function () {
                         recipeObject = this.imageUrlsBySize,
                         recipeArray = Object.keys(recipeObject),
                         recipeArrayLength = recipeArray.length,
-                        recipeImage = recipeObject[recipeArray[recipeArrayLength - 1]];
-                        time = parseFloat(this.totalTimeInSeconds / 60);
-                    $('.Output').append('<div id="recipe-' + recipeId + '" class="recipe"><div><p><strong>' + recipeName + ' </strong><a target="_blank" href="' + recipeUrl + '">See the recipe</a></p><p>This takes about ' + time + ' minutes to make.</p></div><img src="' + recipeImage + '"/></div>');
+                        recipeImage = recipeObject[recipeArray[recipeArrayLength - 1]],
+                        recipeIngredient = this.ingredients,
+                        recipeList = '';
+                    time = parseFloat(this.totalTimeInSeconds / 60);
+                    $('.Output').append('<div id="recipe-' + recipeId + '" class="recipe"><div><p><strong>' + recipeName + ' - </strong><a target="_blank" href="' + recipeUrl + '">See the recipe</a></p><p>This takes about ' + time + ' minutes to make.</p><ul class="ingredients"></ul></div><div><img src="' + recipeImage + '"/></div></div>');
+                    $.each(recipeIngredient, function (i, val) {
+                        recipeList = recipeList + '<li>' + val + '</li>'
+                    });
+                    // $('#recipe-' + recipeId).find('ul').html(recipeList); TODO add this
+                });
+                $('html,body').animate({
+                    scrollTop: $('.Output').offset().top
                 });
             }
         });
@@ -34,7 +43,7 @@ $(document).ready(function () {
     });
 
     $('body').on('click', '.ingredientSearchTrigger', function () {
-        var searched = $(this).closest('.search').find('input').val().trim().replace(/,/g, ''),
+        var searched = $(this).closest('.search').find('input.ingredientSearchText').val().trim().replace(/,/g, ''),
             searchedItem = searched.split(' '), // list of every word searched, split by space bar
             searchTerm = '',
             searchLink = pantryUrl;
